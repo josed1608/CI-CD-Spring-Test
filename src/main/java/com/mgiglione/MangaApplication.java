73f7909fa2e1e26f2ff35fcbf4ac15657a5bddc1
Jose David Vargas <josed1608@gmail.com>
2019-03-21 15:29:35 -0600

    Changes for running on tomcat

--- src/main/java/com/mgiglione/MangaApplication.java
+++ src/main/java/com/mgiglione/MangaApplication.java
@@ -2,13 +2,14 @@ package com.mgiglione;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = { "com.mgiglione" })
 @EnableAsync
-public class MangaApplication {
+public class MangaApplication extends SpringBootServletInitializer {
 
     @Bean
     protected RestTemplate getTemplate() {
